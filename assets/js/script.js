f4b0ae2a94aaf8088cfe64ac7a553979e21a3521
Evrib <130311691+Evrib@users.noreply.github.com>
2023-04-15 19:58:54 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,32 @@
+// import suggestions from './suggestions.js';
 
-// Get a reference to the button element
-var button = document.querySelector("button");
+const generateButton = document.getElementById('generate-button');
+const activitySuggestion = document.getElementById('activity-suggestion');
 
-// Get a reference to the days container element
-var daysContainer = document.getElementById("days");
+function generateSuggestion2() {
+  // Pick a random suggestion from the array
+  const randomIndex = Math.floor(Math.random() * suggestions.length);
+  const suggestion = suggestions[randomIndex];
 
-// Get an array of the day elements
-var dayElements = daysContainer.getElementsByTagName("p");
+  // Update the text and href of the suggestion element
+  const suggestionEl = document.getElementById("activity-suggestion");
+  suggestionEl.textContent = suggestion.text;
+  suggestionEl.href = suggestion.url;
+}
 
-// Add an event listener to the button
-button.addEventListener("click", function() {
-  // Shuffle the dinner options array
-  for (var i = dinnerOptions.length - 1; i > 0; i--) {
-    var j = Math.floor(Math.random() * (i + 1));
-    var temp = dinnerOptions[i];
-    dinnerOptions[i] = dinnerOptions[j];
-    dinnerOptions[j] = temp;
-  }
+function generateSuggestion() {
+  // Pick a random suggestion from the array
+  const randomIndex = Math.floor(Math.random() * suggestions.length);
+  const suggestion = suggestions[randomIndex];
 
-  // Loop through the day elements and update the dinner option for each one
-  for (var i = 0; i < dayElements.length; i++) {
-    var dayElement = dayElements[i];
-    var randomDinnerOption = dinnerOptions[i];
-    var dayText = dayElement.textContent;
-    var currentDinnerOption = dayText.split(" - ")[1]; // Get the current dinner option
-    dayElement.textContent = dayText.replace(" - " + currentDinnerOption, "") + " - " + randomDinnerOption; // Replace the old dinner option with the new one
-  }
-});
+  // Update the text and href of the suggestion element
+  const suggestionEl = document.getElementById("activity-suggestion");
+  suggestionEl.innerHTML = '<a href="' + suggestion.url + '" target="_blank">' + suggestion.text + '</a>';
+}
+
+
+
+generateButton.addEventListener('click', generateSuggestion);
+
+generateSuggestion();
+
---
+++ assets/js/suggestions.js
@@ -0,0 +1,51 @@
+const suggestions = [
+  {
+    text: "Call a friend",
+    url: "https://en.wikipedia.org/wiki/Friendship"
+  },
+  {
+    text: "Go for a walk",
+    url: "https://en.wikipedia.org/wiki/Walking"
+  },
+  {
+    text: "Read a book",
+    url: "https://en.wikipedia.org/wiki/Book"
+  },
+  {
+    text: "Watch a movie",
+    url: "https://en.wikipedia.org/wiki/Film"
+  },
+  {
+    text: "Do some yoga",
+    url: "https://en.wikipedia.org/wiki/Yoga"
+  },
+  {
+    text: "Learn a new language",
+    url: "https://en.wikipedia.org/wiki/Language_learning"
+  },
+  {
+    text: "Cook a meal",
+    url: "https://en.wikipedia.org/wiki/Cooking"
+  },
+  {
+    text: "Go for a bike ride",
+    url: "https://en.wikipedia.org/wiki/Cycling"
+  },
+  {
+    text: "Do some gardening",
+    url: "https://en.wikipedia.org/wiki/Gardening"
+  },
+  {
+    text: "Listen to music",
+    url: "https://en.wikipedia.org/wiki/Music"
+  },
+  {
+    text: "Play a game",
+    url: "https://en.wikipedia.org/wiki/Game"
+  }
+  // Add more suggestions here...
+];
+
+// Export the suggestions array so it can be imported in other files
+// export default suggestions;
+//not applicable???
