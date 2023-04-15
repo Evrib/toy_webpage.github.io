ab193609b898340ac60681d6b41a7d69cbdbb3e1
Evrib <130311691+Evrib@users.noreply.github.com>
2023-04-15 10:31:59 +0200

    Add files via upload

---
+++ _app/immutable/chunks/2.818c1806.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.841dfb74.js";export{t as component};
---
+++ _app/immutable/entry/_page.svelte.841dfb74.js
@@ -0,0 +1,13 @@
+import{S as gt,i as ut,s as dt,k as g,q as s,a as f,l as u,m as it,r as n,h as M,c as m,b as nt,G as a,J as ft,u as p,H as ot}from"../chunks/index.33fbba09.js";function mt(o){let d,v,_,t,k,b=i(o[0])+"",$,E,H,U,c=i(o[0])+"",O,z,A,F,h=i(o[0])+"",P,G,J,K,B=i(o[0])+"",V,N,w,D,R=i(o[0])+"",j,I,L,Q,S=i(o[0])+"",q,W,X,Y,T=i(o[0])+"",C,Z,y,x,tt,et,at,lt,st,rt;return{c(){d=g("button"),v=s("'Säg vad jag ska laga då'"),_=f(),t=g("p"),k=s("Måndag:  "),$=s(b),E=f(),H=g("br"),U=s(` 
+	Tisdag:  `),O=s(c),z=f(),A=g("br"),F=s(` 
+	Onsdag:  `),P=s(h),G=f(),J=g("br"),K=s(` 
+	torsdag: `),V=s(B),N=f(),w=g("br"),D=s(`
+	fredag:  `),j=s(R),I=f(),L=g("br"),Q=s(`
+	lördag:  `),q=s(S),W=f(),X=g("br"),Y=s(`
+	söndag:  `),C=s(T),Z=f(),y=g("br"),x=f(),tt=g("br"),et=f(),at=g("br"),lt=s(" Vald med rättvisa tärningskast, garanterat slumpmässigt")},l(l){d=u(l,"BUTTON",{});var r=it(d);v=n(r,"'Säg vad jag ska laga då'"),r.forEach(M),_=m(l),t=u(l,"P",{});var e=it(t);k=n(e,"Måndag:  "),$=n(e,b),E=m(e),H=u(e,"BR",{}),U=n(e,` 
+	Tisdag:  `),O=n(e,c),z=m(e),A=u(e,"BR",{}),F=n(e,` 
+	Onsdag:  `),P=n(e,h),G=m(e),J=u(e,"BR",{}),K=n(e,` 
+	torsdag: `),V=n(e,B),N=m(e),w=u(e,"BR",{}),D=n(e,`
+	fredag:  `),j=n(e,R),I=m(e),L=u(e,"BR",{}),Q=n(e,`
+	lördag:  `),q=n(e,S),W=m(e),X=u(e,"BR",{}),Y=n(e,`
+	söndag:  `),C=n(e,T),Z=m(e),y=u(e,"BR",{}),x=m(e),tt=u(e,"BR",{}),et=m(e),at=u(e,"BR",{}),lt=n(e," Vald med rättvisa tärningskast, garanterat slumpmässigt"),e.forEach(M)},m(l,r){nt(l,d,r),a(d,v),nt(l,_,r),nt(l,t,r),a(t,k),a(t,$),a(t,E),a(t,H),a(t,U),a(t,O),a(t,z),a(t,A),a(t,F),a(t,P),a(t,G),a(t,J),a(t,K),a(t,V),a(t,N),a(t,w),a(t,D),a(t,j),a(t,I),a(t,L),a(t,Q),a(t,q),a(t,W),a(t,X),a(t,Y),a(t,C),a(t,Z),a(t,y),a(t,x),a(t,tt),a(t,et),a(t,at),a(t,lt),st||(rt=ft(d,"click",o[1]),st=!0)},p(l,[r]){r&1&&b!==(b=i(l[0])+"")&&p($,b),r&1&&c!==(c=i(l[0])+"")&&p(O,c),r&1&&h!==(h=i(l[0])+"")&&p(P,h),r&1&&B!==(B=i(l[0])+"")&&p(V,B),r&1&&R!==(R=i(l[0])+"")&&p(j,R),r&1&&S!==(S=i(l[0])+"")&&p(q,S),r&1&&T!==(T=i(l[0])+"")&&p(C,T)},i:ot,o:ot,d(l){l&&M(d),l&&M(_),l&&M(t),st=!1,rt()}}}function i(o){return o.splice(Math.floor(Math.random()*o.length),1)}function bt(o,d,v){let _=["Fläskpannkaka","Bönröra med bulgur och krämfräsch","Carbonara","kycklingschnitzel","linsgryta","halloumiburgare","Pasta med köttfärsSAUS","ät ute eller ngt idk","Kikärtsgryta","potatismos med falukorv","gaspacho haha nä men fan löksoppa eller ngt","Tacoooosss","Halloumiburgare","Varma mackor"];var t=[],k=0;function b(){k==0&&(v(0,t=_),k++)}return[t,b]}class _t extends gt{constructor(d){super(),ut(this,d,bt,mt,dt,{})}}export{_t as default};
