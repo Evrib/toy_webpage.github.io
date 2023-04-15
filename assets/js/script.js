e0807f1966dcd8a47a7b6d3318d5fb1ff7b90af3
Evrib <130311691+Evrib@users.noreply.github.com>
2023-04-15 15:26:47 +0200

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,18 +1,31 @@
 // Get a reference to the button element
 var button = document.querySelector("button");
 
+// Define an array of possible dinner options
+var dinnerOptions = [	"Fläskpannkaka", 	"Bönröra med bulgur och krämfräsch", 	"Carbonara",	"kycklingschnitzel",	"linsgryta",	"halloumiburgare",	"Pasta med köttfärsSAUS",	"ät ute eller ngt idk",	"Kikärtsgryta",	"potatismos med falukorv",	"gaspacho haha nä men fan löksoppa eller ngt",	"Tacoooosss",	"Halloumiburgare",	"Varma mackor",];
+
+// Get a reference to the days container element
+var daysContainer = document.getElementById("days");
+
+// Get an array of the day elements
+var dayElements = daysContainer.getElementsByTagName("p");
+
 // Add an event listener to the button
 button.addEventListener("click", function() {
-  // Get a reference to the days container element
-  var daysContainer = document.getElementById("days");
+  // Shuffle the dinner options array
+  for (var i = dinnerOptions.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = dinnerOptions[i];
+    dinnerOptions[i] = dinnerOptions[j];
+    dinnerOptions[j] = temp;
+  }
 
-  // Get an array of the day elements
-  var dayElements = daysContainer.getElementsByTagName("p");
-
-  // Loop through the day elements and add "eat dinner!" to each one
+  // Loop through the day elements and update the dinner option for each one
   for (var i = 0; i < dayElements.length; i++) {
     var dayElement = dayElements[i];
+    var randomDinnerOption = dinnerOptions[i];
     var dayText = dayElement.textContent;
-    dayElement.textContent = dayText + " - eat dinner!";
+    var currentDinnerOption = dayText.split(" - ")[1]; // Get the current dinner option
+    dayElement.textContent = dayText.replace(" - " + currentDinnerOption, "") + " - " + randomDinnerOption; // Replace the old dinner option with the new one
   }
 });
